136ff2e18117aa6b83b3c79b22bcbb9588bb5ca0
Alexei Bobrik <fingerloom2004@gmail.com>
2024-04-25 00:51:10 +0300

    FIX: update leisure form

--- src/pages/LeisureAdd/LeisureAdd.tsx
+++ src/pages/LeisureAdd/LeisureAdd.tsx
@@ -1,9 +1,13 @@
 import React, {ReactNode} from 'react'
 import './LeisureAdd.css'
-import {useTitle} from "../../globals";
+import {dayOfWeekNames, useTitle} from "../../globals";
 import {Field, Formik} from "formik";
 import * as Yup from "yup";
 import {LABEL_FOR_INPUTS, PLACEHOLDER_FOR_INPUT} from "../../constants";
+import SecondForm from '../../components/SecondForm/SecondForm';
+import Header from '../../components/Header/Header';
+import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import { DayOfWeek } from '../../models/leisure/DayOfWeek';
 
 const LeisureAdd = () => {
     useTitle("Создание кружка")
@@ -11,47 +15,50 @@ const LeisureAdd = () => {
         nameOfLeisure: Yup.string()
             .required("Название кружка это обязательное поле")
             .min(6, "Название кружка состоит из минимум 6 знаков"),
-        timeOfLeisure: Yup.string()
-            .required("Время это обязательное поле"),
+        timeOfLeisure: Yup.date()
+            .required("Время кружка ялвяется обязательным полем"),
         dayOfLeisure: Yup.string()
             .required("День недели это обязательное поле"),
     }
     return (
-        <div className="container">
-            <Formik
-                validationSchema={Yup.object().shape(shape)}
-                initialValues={
-                    {
-                        nameOfLeisure: "",
-                        timeOfLeisure: new Date(),
-                        dayOfLeisure: new Date()
+        <>
+            <Header />
+            <NavigationBar />
+            <div className="sec-container">
+                <Formik
+                    validationSchema={Yup.object().shape(shape)}
+                    initialValues={
+                        {
+                            nameOfLeisure: "",
+                            timeOfLeisure: new Date(),
+                            dayOfLeisure: new Date(),
+                        }
                     }
-                }
-                onSubmit={(values: { nameOfLeisure: string, timeOfLeisure: Date, dayOfLeisure: Date }) => {
-                    console.log(values)
-                }}
-            >
-                {({
-                      values,
-                      errors,
-                      touched,
-                      handleChange,
-                      handleBlur,
-                      handleSubmit
-                  }) => (
-                    <form className="screen-1" noValidate onSubmit={handleSubmit}>
-                        <div className="password">
-                            <label htmlFor="nameOfLeisure">{LABEL_FOR_INPUTS["nameOfLeisure"]}:</label>
-                            <div key="nameOfLeisure">
-                                <div className="sec-2">
+                    onSubmit={(values: { nameOfLeisure: string, timeOfLeisure: Date, dayOfLeisure: Date }) => {
+                        console.log(values)
+                    }}
+                >
+                    {({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit
+                    }) => (
+                        <SecondForm noValidate onSubmit={handleSubmit}>
+                            <div className="form-field-container">
+                                <label
+                                    htmlFor="nameOfLeisure">{LABEL_FOR_INPUTS["nameOfLeisure"]}:</label>
+                                <div className="input-container">
                                     <input
-                                        type="nameOfLeisure"
+                                        type="text"
                                         name="nameOfLeisure"
                                         onChange={handleChange}
                                         onBlur={handleBlur}
                                         value={values["nameOfLeisure"]}
                                         placeholder={PLACEHOLDER_FOR_INPUT["nameOfLeisure"]}
-                                        className="form-control inp_text"
+                                        className=""
                                         id="nameOfLeisure"
                                     />
                                 </div>
@@ -59,12 +66,11 @@ const LeisureAdd = () => {
                                     {errors["nameOfLeisure"] && touched["nameOfLeisure"] && errors["nameOfLeisure"]}
                                 </p>
                             </div>
-                        </div>
 
-                        <div className="password">
-                            <label htmlFor="timeOfLeisure">{LABEL_FOR_INPUTS["timeOfLeisure"]}:</label>
-                            <div key="timeOfLeisure">
-                                <div className="sec-2">
+                            <div className="form-field-container">
+                                <label
+                                    htmlFor="timeOfLeisure">{LABEL_FOR_INPUTS["timeOfLeisure"]}:</label>
+                                <div className="input-container">
                                     <input
                                         type="time"
                                         name="timeOfLeisure"
@@ -72,44 +78,39 @@ const LeisureAdd = () => {
                                         onBlur={handleBlur}
                                         value={values["timeOfLeisure"].toString()}
                                         placeholder={PLACEHOLDER_FOR_INPUT["timeOfLeisure"]}
-                                        className="form-control inp_text"
+                                        className=""
                                         id="timeOfLeisure"
                                     />
                                 </div>
                                 <p className="error">
-                                    {errors["timeOfLeisure"] as ReactNode && touched["timeOfLeisure"] as ReactNode && errors["timeOfLeisure"] as ReactNode}
+                                    {(errors["timeOfLeisure"] as ReactNode) && (touched["timeOfLeisure"] as ReactNode) && (errors["timeOfLeisure"] as ReactNode)}
                                 </p>
                             </div>
-                        </div>
 
+                            <div className="form-field-container">
+                                <label
+                                    htmlFor="dayOfLeisure">{LABEL_FOR_INPUTS["dayOfLeisure"]}:</label>
+                                <div className="input-container">
 
-                        <div className="password">
-                            <label htmlFor="dayOfLeisure">{LABEL_FOR_INPUTS["dayOfLeisure"]}:</label>
-                            <div key="dayOfLeisure">
-                                <div className="sec-2">
-                                    <Field as="select" id="dayOfLeisure" name="dayOfLeisure">
-                                        <option value="">Выберите день недели</option>
-                                        <option value="Понедельник">Понедельник</option>
-                                        <option value="Вторник">Вторник</option>
-                                        <option value="Среда">Среда</option>
-                                        <option value="Четверг">Четверг</option>
-                                        <option value="Пятница">Пятница</option>
-                                        <option value="Суббота">Суббота</option>
-                                        <option value="Воскресенье">Воскресенье</option>
+                                    <Field as="select" id="dayOfLeisure" name="dayOfLeisure" placeholder="Выберите день недели">
+                                        {(Object.keys(DayOfWeek) as Array<keyof typeof DayOfWeek>).map(key => (
+                                            <option key={key} value={dayOfWeekNames[DayOfWeek[key]]}>{dayOfWeekNames[DayOfWeek[key]]}</option>
+                                        ))}
                                     </Field>
+                                
+                                    <p className="error">
+                                        {errors["dayOfLeisure"] as ReactNode && touched["dayOfLeisure"] as ReactNode && errors["dayOfLeisure"] as ReactNode}
+                                    </p>
                                 </div>
-                                <p className="error">
-                                    {errors["dayOfLeisure"] as ReactNode && touched["dayOfLeisure"] as ReactNode && errors["dayOfLeisure"] as ReactNode}
-                                </p>
                             </div>
-                        </div>
 
-                        <button className="login" type="submit">Создать</button>
-                    </form>
+                            <button className="btn btn-primary" type="submit">Создать кружок</button>
+                        </SecondForm>
 
-                )}
-            </Formik>
-        </div>
+                    )}
+                </Formik>
+            </div>
+        </>
     )
 }
 
